b9d9967c2391802098883ac67a95e50936a1e598
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-10-30 08:46:48 +0100

    created folder photo &product

---
+++ Salah-Site-React/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+      <div className="about">
+          <h2>About Me</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repudiandae, similique culpa illo eveniet provident quasi a eos voluptate sapiente molestiae excepturi, cumque, dolores velit mollitia accusamus eligendi quibusdam fugiat.</p>
+          <p>Quia eveniet modi nisi aliquid a magni maxime ab ducimus placeat tempore nesciunt expedita hic saepe, labore numquam repellendus iure. Impedit vero adipisci quis atque fugit? Adipisci vero vitae laudantium.</p>
+          <p>Minus, iste. Minima deleniti velit est explicabo officia exercitationem, quasi provident non, obcaecati architecto nemo consequatur eum at excepturi? Ad, ab. Dolore doloremque id a ratione. Nulla obcaecati aspernatur blanditiis.</p>
+          <p>Harum iure aspernatur quaerat rerum quibusdam fuga placeat ea tenetur numquam libero, similique illo, iusto repellat ipsa voluptatem eaque nisi deleniti odit autem blanditiis quas ipsam provident molestiae perspiciatis? Aliquam.</p>
+          <p>Veritatis voluptas veniam explicabo numquam obcaecati maxime dignissimos consectetur. Sunt laboriosam voluptate magni impedit rem sequi, soluta officiis consequuntur deserunt ipsa blanditiis ea quas, amet cumque necessitatibus id ad corporis.</p>
+    </div>
+  )
+}
+
+export default About

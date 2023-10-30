0a832fca6f9eee20595b7934cffadb43a0e3e866
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-10-30 08:54:09 +0100

    notfound.jsx

---
+++ Salah-Site-React/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+      <div className="error">
+          <h2>Page Not Found! 404</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, repellat, laborum consequatur quae cumque laboriosam iure explicabo dignissimos quod, culpa est eligendi quidem aperiam dolorum atque voluptatum facere eos autem!</p>
+          <p>Quidem ullam vel porro ut amet, dolores saepe ipsam rerum nostrum inventore non asperiores mollitia earum harum dicta quas officia quis fugit deserunt quam eligendi nesciunt! Veritatis ipsam dolores id!</p>
+    </div>
+  )
+}
+
+export default NotFound

3123db781b24ea5efdc24460ea63502b18b541ed
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-05 22:29:45 +0100

    Update main.jsx

--- Salah-Site-React/src/main.jsx
+++ Salah-Site-React/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+import './index2.css'
+import DataContextProvider from './context/DataContextProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
   </React.StrictMode>,
 )

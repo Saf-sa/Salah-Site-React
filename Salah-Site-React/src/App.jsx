863eb1e86da5003a613b9d6739e26c023fd845e1
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-05 22:26:42 +0100

    Update App.jsx

--- Salah-Site-React/src/App.jsx
+++ Salah-Site-React/src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import RootLayout from "./layouts/RootLayout";
+import PhotoLayout from "./layouts/photoLayout";
+import { BrowserRouter as Router, } from 'react-router-dom';
+
+
+// styles
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Music from "./pages/Music";
+import Film from "./pages/Film";
+import Model from "./pages/Model";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+import MusicPhoto from './pages/MusicPhoto';
+import SportPhoto from './pages/SportPhoto';
+import SkyPhoto from './pages/SkyPhoto';
+import TownPhoto from './pages/TownPhoto';
+import GayaPhoto from './pages/GayaPhoto';
+import HumanPhoto from './pages/HumanPhoto';
+
+import Products from "./pages/products/Products";
+
+import { productsLoader } from "./pages/products/Products";
+import ProductDetails, {
+  fetchProductDetails,
+} from "./pages/products/ProductDetails";
+import ProductsLayout from "./layouts/ProductsLayout";
+import CartDetails from "./pages/products/CartDetails";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="music" element={<Music />} />
+      <Route path="film" element={<Film />} />
+      <Route path="photo" element={<PhotoLayout />} />
+      <Route path="model" element={<Model />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="products" element={<ProductsLayout />}>
+        <Route element={<Products />} loader={productsLoader} />
+        <Route path="cart" element={<CartDetails />} />
+        <Route
+          path=":pid"
+          element={<ProductDetails />}
+          loader={fetchProductDetails}
+        />
+      </Route>
+
+      <Route path="photo" element={<PhotoLayout />}>
+        <Route path="music" element={<MusicPhoto />} />
+        <Route path="human" element={<HumanPhoto />} />
+        <Route path="sport" element={<SportPhoto />} />
+        <Route path="sky" element={<SkyPhoto />} />
+        <Route path="town" element={<TownPhoto />} />
+        <Route path="gaya" element={<GayaPhoto />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;

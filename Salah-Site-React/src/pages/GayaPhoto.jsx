330affc6579939b826b5309f501b0069a9f76f0e
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-09 08:42:03 +0100

    created gayaphoto.jsx

---
+++ Salah-Site-React/src/pages/GayaPhoto.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../style/photography.css";
+
+function Gaya() {
+  return (
+    <div className='Photo'>
+
+   <h2>Gaya</h2>
+    
+    </div>
+  )
+}
+
+export default Gaya

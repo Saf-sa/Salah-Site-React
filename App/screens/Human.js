4af3152c1fd7111da9582588b7e0f4182931720e
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-23 15:10:42 +0100

    basic styled human.js

--- App/screens/Human.js
+++ App/screens/Human.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 import React from 'react'
 
 const Human = () => {
@@ -12,8 +12,15 @@ const Human = () => {
         backgroundColor: "green",
       }}
     >
-      <Text>Photo Human</Text>
+      <Text style={styles.title}>Human Screen</Text>
     </View>
-)}
+  );}
 
 export default Human
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    color: "white",
+  },
+});

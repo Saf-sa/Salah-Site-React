7bad9566bd495197f5ff79d886da7a1d34125aa0
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-23 15:07:38 +0100

    basic styled film.js

--- App/screens/Contact.js
+++ App/screens/Contact.js
@@ -18,6 +18,7 @@ const Contact = () => {
 }
 
 export default Contact
+
 const styles = StyleSheet.create({
   title: {
     fontSize: 30,
--- App/screens/Film.js
+++ App/screens/Film.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 import React from 'react'
 
 const Film = () => {
@@ -9,12 +9,19 @@ const Film = () => {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "yellow",
+        backgroundColor: "brown",
       }}
     >
-      <Text>Film</Text>
+      <Text style={styles.title}>Film Screen</Text>
     </View>
   );
 }
 
 export default Film
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    color: "white",
+  },
+});

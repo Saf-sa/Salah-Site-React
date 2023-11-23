ee745e61a68caf76c29d2166aa542d10a4240b24
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-23 15:20:12 +0100

    basic styled sport.js

--- App/screens/Sport.js
+++ App/screens/Sport.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 import React from 'react'
 
 const Sport = () => {
@@ -12,8 +12,15 @@ const Sport = () => {
         backgroundColor: "green",
       }}
     >
-      <Text>Sport</Text>
+      <Text style={styles.title}>Sport Screen</Text>
     </View>
   );
 }
 export default Sport
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    color: "white",
+  },
+});

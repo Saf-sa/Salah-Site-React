0ad859bcb2474ba71edbbe17904d55c7f7420e1f
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-23 15:08:58 +0100

    basic styled gaya.js

--- App/screens/Gaya.js
+++ App/screens/Gaya.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 import React from 'react'
 
 const Gaya = () => {
@@ -12,9 +12,16 @@ const Gaya = () => {
         backgroundColor: "blue",
       }}
     >
-      <Text>Gaya</Text>
+      <Text style={styles.title}>Gaya Screen</Text>
     </View>
   );
 }
 
 export default Gaya
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    color: "white",
+  },
+});
